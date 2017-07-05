f4f7569dda70e4127dc59af00b14bf1b5a9f2495
LudovicGuerra <ludovic791@hotmail.fr>
2017-07-05 17:48:25 +0200

    First commit

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import itertools
+import pytmx
+from kivy import Config
 from kivy.app import App
+from kivy.uix.image import Image
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ReferenceListProperty,\
-    ObjectProperty
+from kivy.properties import NumericProperty, ReferenceListProperty, \
+    ObjectProperty, Clock, Logger
 from kivy.vector import Vector
-from kivy.clock import Clock
+import tiled
+from kivy.core.image import Image as CoreImage
+
 
 
 class PongPaddle(Widget):
@@ -27,14 +33,74 @@ class PongBall(Widget):
         self.pos = Vector(*self.velocity) + self.pos
 
 
+class TileSet(Widget):
+    tmxdata = pytmx.TiledMap("data/Map/test01.tmx")
+    image = tmxdata.get_tile_image(0, 0, 0)
+    def test(self):
+        pass
+
 class PongGame(Widget):
     ball = ObjectProperty(None)
     player1 = ObjectProperty(None)
     player2 = ObjectProperty(None)
 
+    def image(self, path, colorkey, tileset):
+        Config.set("kivy", "log_level", "debug")
+        tile_image_path = "data/Map" + '/' + tileset.source
+        print(tile_image_path)
+        Logger.debug('KivyTiledMap: loading tile image at {}'.format(tile_image_path))
+        Image(source=tile_image_path)
+        texture = CoreImage(tile_image_path).texture
+
+        tileset.width, tileset.height = texture.size
+        tilewidth = tileset.tilewidth + tileset.spacing
+        tileheight = tileset.tileheight + tileset.spacing
+        Logger.debug('KivyTiledMap: TiledTileSet: {}x{} with {}x{} tiles'.format(tileset.width, tileset.height, tilewidth, tileheight))
+
+        # some tileset images may be slightly larger than the tile area
+        # ie: may include a banner, copyright, ect.  this compensates for that
+        width = int((((tileset.width - tileset.margin * 2 + tileset.spacing) / tilewidth) * tilewidth) - tileset.spacing)
+        height = int((((tileset.height - tileset.margin * 2 + tileset.spacing) / tileheight) * tileheight) - tileset.spacing)
+        Logger.debug('KivyTiledMap: TiledTileSet: true size: {}x{}'.format(width, height))
+
+        # initialize the image array
+        Logger.debug('KivyTiledMap: initializing image array')
+        self.images = [0] * 100
+
+        p = itertools.product(
+            range(tileset.margin, height + tileset.margin, tileheight),
+            range(tileset.margin, width + tileset.margin, tilewidth)
+        )
+
+        # trim off any pixels on the right side that isn't a tile
+        # this happens if extra graphics are included on the left, but they are not actually part of the tileset
+        width -= (tileset.width - tileset.margin) % tilewidth
+
+        for real_gid, (y, x) in enumerate(p, tileset.firstgid):
+            if x + tileset.tilewidth - tileset.spacing > width:
+                continue
+
+            gids = self.map_gid(real_gid)
+
+            if gids:
+                # invert y for OpenGL coordinates
+                y = tileset.height - y - tileset.tileheight
+
+                tile = texture.get_region(x, y, tileset.tilewidth, tileset.tileheight)
+
+                for gid, flags in gids:
+                    self.images[gid] = tile
+
     def serve_ball(self, vel=(4, 0)):
         self.ball.center = self.center
         self.ball.velocity = vel
+
+        tmx_data= pytmx.TiledMap("data/Map/test01.tmx", self.image)
+        print(tmx_data.get_tile_properties(0,0,0))
+
+        tmx_data.get_tile_image(0, 0, 0)
+
+
 
     def update(self, dt):
         self.ball.move()
@@ -66,6 +132,7 @@ class PongApp(App):
     def build(self):
         game = PongGame()
         game.serve_ball()
+
         Clock.schedule_interval(game.update, 1.0 / 60.0)
         return game
 
---
+++ tiled_map.py
@@ -0,0 +1,8 @@
+import pytmx
+from kivy.uix.image import Image
+
+
+class TiledMap():
+
+    def __init__(self):
+        pass
